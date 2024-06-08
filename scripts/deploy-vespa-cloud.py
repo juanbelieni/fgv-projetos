ff3069a174bb7ee6a364baec06f7e4098aa1fcc2
Eduardo Adame <eadamesalles@gmail.com>
2024-06-08 18:48:31 -0300

    Fixing endpoints

--- scripts/deploy-vespa-cloud.py
+++ scripts/deploy-vespa-cloud.py
@@ -20,13 +20,6 @@ if __name__ == "__main__":
 
     os.environ["TENANT_NAME"] = "crazyfrogger"  # Replace with your tenant name
     application = "crazyfrogger"
-    vespa_cli_command = (
-        f'vespa config set application {os.environ["TENANT_NAME"]}.{application}'
-    )
-    
-    os.system("vespa config set target cloud")
-    os.system(vespa_cli_command)
-    os.system("vespa auth cert -N")
     
     cert_path = (
         Path.home()
@@ -43,8 +36,6 @@ if __name__ == "__main__":
         print(
             "ERROR: set the correct paths to security credentials. Correct paths above and rerun until you do not see this error"
         )
-
-    os.system("vespa auth api-key")
     
     api_key_path = Path.home() / ".vespa" / f"{os.environ['TENANT_NAME']}.api-key.pem"
     
