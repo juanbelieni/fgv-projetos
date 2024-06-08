1be52b9fd31979f126bd8e66547fd73b668ec57d
Eduardo Adame <eadamesalles@gmail.com>
2024-06-08 18:22:28 -0300

    Expurging Poetry commands and adding generate keys

--- scripts/generate-keys.py
+++ scripts/generate-keys.py
@@ -1,20 +1,7 @@
-import tempfile
 import os
 
 from os.path import exists
 from pathlib import Path
-from utils.vespa import vespa_app_package
-from vespa.deployment import VespaCloud
-
-def read_secret():
-    """Read the API key from the environment variable. This is
-    only used for CI/CD purposes."""
-    t = os.getenv("VESPA_TEAM_API_KEY")
-    if t:
-        return t.replace(r"\n", "\n")
-    else:
-        return t
-
 
 if __name__ == "__main__":
 
@@ -47,21 +34,3 @@ if __name__ == "__main__":
     os.system("vespa auth api-key")
     
     api_key_path = Path.home() / ".vespa" / f"{os.environ['TENANT_NAME']}.api-key.pem"
-    
-    print("[Deploying Vespa Docker container with the config]")
-
-    vespa_cloud = VespaCloud(
-        tenant=os.environ["TENANT_NAME"],
-        application=application,
-        key_content=read_secret() if read_secret() else None,
-        key_location=api_key_path,
-        application_package=vespa_app_package,
-    )
-    
-    app = vespa_cloud.deploy()
-    
-    endpoint = vespa_cloud.get_mtls_endpoint()
-    
-    print(f"Endpoint: {endpoint}")
-    
-    os.environ["ENDPOINT"] = endpoint
